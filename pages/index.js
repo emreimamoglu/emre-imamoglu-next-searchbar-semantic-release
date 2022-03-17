ecd103cdb0e797f9d47807d1166c04718df9b905
Emre Imamoglu <e.imamoglu@azerion.com>
2022-03-17 05:13:57 +0300

    Add husky and lint-staged

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,6 @@ import Image from 'next/image'
 import SearchBar from '../components/Searchbar/Searchbar'
 export default function Home() {
   return (
-   <SearchBar/>
+   <SearchBar scale={0.7} searchBoxBackground="#1776D1" searchIconBackground='#1776D1' searchIconColor='white' />
   )
 }
